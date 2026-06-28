8d7ccfac68b203e442026041055cc57d64fa9e78
commander150 <42314898+commander150@users.noreply.github.com>
2026-06-28 18:08:53 +0200

    Deprecate unsafe Godot package copy runner

--- Aeterna game engine/python/copy_package_to_godot.bat
+++ Aeterna game engine/python/copy_package_to_godot.bat
@@ -1,4 +1,6 @@
 @echo off
 cd /d "%~dp0"
-robocopy ".\sample_runtime_package" "..\Godot\runtime_package" /E
-pause
+echo Deprecated: this script no longer performs unsafe direct copy.
+echo Use publish_runtime_package_to_godot.bat for validated publish.
+call publish_runtime_package_to_godot.bat %*
+exit /b %ERRORLEVEL%
